69657e0b6080b9e228a73379c30e5d8a884daa1a
mstrlegend <92732442+mstrlegend@users.noreply.github.com>
2023-10-09 06:48:32 +0000

    ..

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import re
 # {"certPinnedAppList":[],"status":"success","steering_config_name":"Netskope_test_group"}
 # Goal is to get the appname and app domains
 """
-while True:
-    with open("Text-File.txt", "r") as r, open("Output", "a+") as f:
-        print(re.findall("{(.*)},", r))
+with open("Text-File.txt", "r") as r:
+    while True:
+        action_an = str(re.findall("{(.*)},", r.read()))
+        appName = str(re.search(r"\"appName\":\"(.*)\",\"app_domains\"", action_an).groups(1)).strip("(',')")
+        print(appName)
 """
 
-x = '{"action":1,"appName":"Amazon CloudDrive","app_domains":["drive.amazon.com","cdws.us-east-1.amazonaws.com","cdws.us-west-2.amazonaws.com","drive.amazonaws.com"],"mode":"tunnel","processName":"AmazonCloudDrive.exe","rowID":"151","tunnel_domains":["drive.amazonaws.com","cdws.us-east-1.amazonaws.com","cdws.us-west-2.amazonaws.com","drive.amazon.com"]}'
+x = '{"action":1,"appName":"Amazon CloudDrive","app_domains":["drive.amazon.com","cdws.us-east-1.amazonaws.com","cdws.us-west-2.amazonaws.com","drive.amazonaws.com"],"mode":"tunnel","processName":"AmazonCloudDrive.exe","rowID":"151","tunnel_domains":["drive.amazonaws.com","cdws.us-east-1.amazonaws.com","cdws.us-west-2.amazonaws.com","drive.amazon.com"]},{"action":1,"appName":"[NTG_NoInternet_Apps_Bypass]","app_domains":["*"],"isRegex":"false","mode":"direct","processName":"Bitsadmin.exe","rowID":"custom_11","tunnel_domains":[]},{"action":1,"appName":"[NTG_NoInternet_Apps_Bypass]","app_domains":["*"],"isRegex":"false","mode":"direct","processName":"CertOC.exe","rowID":"custom_11","tunnel_domains":[]},{"action":1,"appName":"[NTG_NoInternet_Apps_Bypass]","app_domains":["*"],"isRegex":"false","mode":"direct","processName":"CertReq.exe","rowID":"custom_11","tunnel_domains":[]},{"action":1,"appName":"[NTG_NoInternet_Apps_Bypass]","app_domains":["*"],"isRegex":"false","mode":"direct","processName":"Certutil.exe","rowID":"custom_11","tunnel_domains":[]},{"action":1,"appName":"[NTG_NoInternet_Apps_Bypass]","app_domains":["*"],"isRegex":"false","mode":"direct","processName":"Cmd.exe","rowID":"custom_11","tunnel_domains":[]},'
 
-action_an = str(re.findall("{(.*)}", x))
-appName = str(re.search(r"\"appName\":\"(.*)\",\"app_domains\"", action_an).groups(1)).strip("(',')")
-print(appName)
+action_an = str(re.findall("{(.*)}", x)).split()
+appName = str(re.search(r"\"appName\":\"(.*)\",\"app_domains\"", action_an).groups(1))
+print(appName + "\n")
 
-appDomains = str(re.search(r"\"app_domains\":\[(.*\")],\"mode\"", action_an).groups(1)).strip("(',')")
-print(appDomains)
+appDomains = str(re.search(r"\"app_domains\":\[(.*\")],\"mode\"", action_an).groups(1))
+print(appDomains + "\n")
