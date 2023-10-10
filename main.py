b6225b9ad13aa727c7fcde7ec36f088bf48b94b7
mstrlegend <92732442+mstrlegend@users.noreply.github.com>
2023-10-10 01:35:43 +0000

    ..

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-# {"certPinnedAppList":[],"status":"success","steering_config_name":"Netskope_test_group"}
-# Goal is to get the appname and app domains
+# {"certPinnedAppList":[*all configs are stored here*],"status":"success","steering_config_name":"Netskope_test_group"} is the header of the file
+# Goal is to get the the below features from a standard netskope nsbypass config file
 
 # The following are dictionary options: 'action' 'appName' 'app_domains' 'mode' 'processName' 'rowID' 'tunnel_domains'
 
@@ -9,26 +9,21 @@ def read_file():
     with open("nsbypass.json", "r") as r:
         index = 0
         output = ""
-        x = eval(r.read())
+        x = eval(re.search(r"certPinnedAppList\":\[(.*)]\,\"status\":\"success\"", r.read()).group(1))
         for i in range(len(x)):
             z = x[index]
-            if z['app_domains'] == ['*']:
-                z['app_domains'] = "No Domains"
-            if z['tunnel_domains'] == [] or ['*']:
-                z['tunnel_domains'] = "No tunnel domains"
-            output += f"{z['mode']}: {z['appName']} {z['processName']}\n"
+            output += f"{z['mode']}: {z['appName']} - {z['processName']}\n"
             index += 1
     r.close
     with open("Output.txt", 'w') as w:
         w.write("Netskope_test_group:\n\n" + "Mode | App Name | Process Name\n" + output)
     w.close
     return "Output has been written to: 'Output.txt'"
-    
 
 print(read_file())
 
 with open("Output.txt", "r") as r:
     for line in r.readlines():
-        if bool(re.search("^tunnel", line)) is True:
+        if bool(re.search("^direct", line)) is True:
             print(line, end="")
 
